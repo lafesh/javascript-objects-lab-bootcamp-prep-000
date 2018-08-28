78121e94f66fa1312a1599cb77a5305f1f247f90
Gina Fedder <ginafedder@yahoo.com>
2018-08-28 16:43:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
-  return Object.assign ({}, recipes, {'key': value});
+  return Object.assign ({}, recipes, {key: value});
 }
